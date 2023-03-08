58b256b6bcc65cb1ab8c25f4071b9246b8c85667
Spazi <talishmarprkle+g@gmail.com>
2023-03-08 18:45:57 +0200

    Package updates, folder restructuring

---
+++ Assets/Scripts/GlobalShaderCameraControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalShaderCameraControl : MonoBehaviour
+{
+	[SerializeField] Transform wallFadeTarget;
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
